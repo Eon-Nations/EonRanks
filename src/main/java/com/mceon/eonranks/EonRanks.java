217384d7a83eb4e7f4a4e603b263e094d4199a21
Colin Grebenstein <72942847+colin-grebenstein@users.noreply.github.com>
2022-01-02 06:06:17 -0700

    Register RankMenuIteraction.java in Main

--- src/main/java/com/mceon/eonranks/EonRanks.java
+++ src/main/java/com/mceon/eonranks/EonRanks.java
@@ -1,5 +1,6 @@
 package com.mceon.eonranks;
 
+import com.mceon.eonranks.ranks.RankMenuInteraction;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EonRanks extends JavaPlugin {
@@ -7,11 +8,15 @@ public final class EonRanks extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        registerRanks();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private void registerRanks() {
+        new RankMenuInteraction(this);
     }
 }
