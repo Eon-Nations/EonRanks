4032bb8916ee493c0be90b1603ccfa14c28a0937
Colin Grebenstein <72942847+colin-grebenstein@users.noreply.github.com>
2022-01-02 21:22:10 -0700

    More debug statements

--- src/main/java/com/mceon/eonranks/ranks/TestMetadata.java
+++ src/main/java/com/mceon/eonranks/ranks/TestMetadata.java
@@ -1,10 +1,10 @@
 package com.mceon.eonranks.ranks;
 
 import com.mceon.eonranks.EonRanks;
-import net.kyori.adventure.text.Component;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.cacheddata.CachedMetaData;
 import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.Node;
 import net.luckperms.api.node.NodeType;
 import net.luckperms.api.node.types.MetaNode;
 import org.bukkit.Bukkit;
@@ -12,6 +12,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.util.Collection;
 
 public class TestMetadata implements Listener {
 
@@ -30,6 +32,9 @@ public class TestMetadata implements Listener {
         e.getPlayer().sendMessage("Your level is " + metaData.getMetaValue("level", Integer::parseInt).orElse(-1));
 
         User user = luckPermsAPI.getUserManager().getUser(e.getPlayer().getUniqueId());
+        Collection<Node> nodeCollection = user.data().toCollection();
+        nodeCollection.forEach(node -> e.getPlayer().sendMessage("Node: " + node.getKey()));
+
         MetaNode node = MetaNode.builder("level", Integer.toString(1)).build();
         user.data().clear(NodeType.META.predicate(mn -> mn.getMetaKey().equals("level")));
         user.data().add(node);
