64cb67a4d66c89ba672c82c484a925d5b956c393
Colin Grebenstein <72942847+colin-grebenstein@users.noreply.github.com>
2022-01-02 21:00:20 -0700

    Updated Testing Display

--- src/main/java/com/mceon/eonranks/ranks/TestMetadata.java
+++ src/main/java/com/mceon/eonranks/ranks/TestMetadata.java
@@ -1,11 +1,14 @@
 package com.mceon.eonranks.ranks;
 
 import com.mceon.eonranks.EonRanks;
+import net.kyori.adventure.text.Component;
 import net.luckperms.api.LuckPerms;
+import net.luckperms.api.cacheddata.CachedMetaData;
 import net.luckperms.api.model.user.User;
 import net.luckperms.api.node.NodeType;
 import net.luckperms.api.node.types.MetaNode;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -27,5 +30,8 @@ public class TestMetadata implements Listener {
         MetaNode node = MetaNode.builder("level", Integer.toString(1)).build();
         user.data().clear(NodeType.META.predicate(mn -> mn.getMetaKey().equals("level")));
         user.data().add(node);
+
+        CachedMetaData metaData = luckPermsAPI.getPlayerAdapter(Player.class).getMetaData(e.getPlayer());
+        e.getPlayer().sendMessage("Your level is " + metaData.getMetaValue("level", Integer::parseInt).orElse(-1));
     }
 }
